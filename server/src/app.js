cfd53131495f5297322f893253cdf072075d8588
Golam Mostafa <mostafarmstu@gmail.com>
2022-07-24 15:34:43 +0600

    update

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
+// const cors = require('cors');
 const morgan = require('morgan');
 const planetsRouter = require('./routes/planets/planets.router.js');
 const { launchesRouter } = require('./routes/launches/launches.router.js');
 
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-}))
+// app.use(cors({
+//     origin: 'http://localhost:5000',
+// }))
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use(morgan('combined'));
 
