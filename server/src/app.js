2b6bc35c90cf89439ed39dfc491f745cdbda37b8
Golam Mostafa <mostafarmstu@gmail.com>
2022-07-24 15:50:36 +0600

    update

--- server/src/app.js
+++ server/src/app.js
@@ -9,14 +9,11 @@ const app = express();
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    if ('OPTIONS' == req.method) {
-       res.sendStatus(200);
-     }
-     else {
-       next();
-     }});
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin,Content-Type, Authorization, x-id, Content-Length, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
 
 // app.use(cors({
 //     origin: 'https://rocky-fjord-66825.herokuapp.com',
